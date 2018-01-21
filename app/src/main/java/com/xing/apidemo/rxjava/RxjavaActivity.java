81e5baf8c9be07c9efc65958375f3942fb8bf84c
zhaochuanxing <zhaochuanxing@gmail.com>
2018-01-21 15:24:10 +0800

    add subject

--- app/src/main/java/com/xing/apidemo/rxjava/RxjavaActivity.java
+++ app/src/main/java/com/xing/apidemo/rxjava/RxjavaActivity.java
@@ -14,6 +14,10 @@ import rx.Subscriber;
 import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.plugins.RxJavaCompletableExecutionHook;
+import rx.subjects.AsyncSubject;
+import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 
 public class RxjavaActivity extends AppCompatActivity {
 
@@ -29,7 +33,12 @@ public class RxjavaActivity extends AppCompatActivity {
         createObervable();
 //        subscribe();
 //        createAction();
-        createInterval();
+//        createInterval();
+
+//        doAsyncSubject();
+//        doBehaviorSubject();
+//        doPublisSubject();
+        doReplySubject();
     }
 
     // 创建观察者(订阅者） 同observer
@@ -165,5 +174,104 @@ public class RxjavaActivity extends AppCompatActivity {
                 });
     }
 
+    /**
+     * https://mcxiaoke.gitbooks.io/rxdocs/content/Subject.html
+     *
+     * Subject可以看成是一个桥梁或者代理，在某些ReactiveX实现中（如RxJava），它同时充当了Observer和Observable的角色。因为它是一个Observer，它可以订阅一个或多个Observable；又因为它是一个Observable，它可以转发它收到(Observe)的数据，也可以发射新的数据。
+     由于一个Subject订阅一个Observable，它可以触发这个Observable开始发射数据（如果那个Observable是"冷"的--就是说，它等待有订阅才开始发射数据）。因此有这样的效果，Subject可以把原来那个"冷"的Observable变成"热"的。
+
+     */
+
+
+    /**
+     * 一个AsyncSubject只在原始Observable完成后，发射来自原始Observable的最后一个值。
+     * （如果原始Observable没有发射任何值，AsyncObject也不发射任何值）它会把这最后一个值发射给任何后续的观察者。
+     *
+     * https://www.jianshu.com/p/1257c8ba7c0c
+     */
+    private void doAsyncSubject(){
+        //值得注意的是一定要用Subcect.create()的方式创建并使用，不要用just(T)、from(T)、create(T)创建，否则会导致失效...
+        //// 因为just(T)、from(T)、create(T)会把Subject转换为Obserable
+        AsyncSubject<Integer> as = AsyncSubject.create();
+        //触发事件
+        as.onNext(1);
+        as.onNext(2);
+        as.onNext(3);
+        //结束后订阅收到 3
+        //无论输入多少参数，永远只输出最后一个参数。
+        as.onCompleted();
+
+        as.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer value) {
+                Log.i(TAG,"doAsyncSubject "+value);
+            }
+        });
+
+    }
+
+    /**
+     * 当观察者订阅BehaviorSubject时，它开始发射原始Observable最近发射的数据（如果此时还没有收到任何数据，它会发射一个默认值）
+     * ，然后继续发射其它任何来自原始Observable的数据。
+     */
+    private void doBehaviorSubject(){
+        BehaviorSubject<Integer> behaviorSubject = BehaviorSubject.create(1);
+//        behaviorSubject.onNext(1);
+//        behaviorSubject.onNext(2);
+//        behaviorSubject.onNext(3);
+        behaviorSubject.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.i(TAG,"doBehaviorSubject "+integer);
+            }
+        });
+        behaviorSubject.onNext(4);
+        behaviorSubject.onNext(5);
+        behaviorSubject.onCompleted();
+        //根据上面的说明 输出为1 4 5
+    }
+
+    /**
+     * PublishSubject只会把在订阅发生的时间点之后来自原始Observable的数据发射给观察者。
+     * PublishSubject可能会一创建完成就立刻开始发射数据（除非你可以阻止它发生）
+     * ，因此这里有一个风险：在Subject被创建后到有观察者订阅它之前这个时间段内，一个或多个数据可能会丢失。如果要确保来自原始Observable的所有数据都被分发，你需要这样做：或者使用Create创建那个Observable以便手动给它引入"冷"Observable的行为（当所有观察者都已经订阅时才开始发射数据）
+     */
+    private void doPublisSubject(){
+        PublishSubject<Integer> publishSubject = PublishSubject.create();
+        publishSubject.onNext(11);
+        publishSubject.onNext(12);
+        publishSubject.onNext(13);
+//        publishSubject.onCompleted();
+        publishSubject.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.i(TAG,"doPublisSubject "+integer);
+            }
+        });
+        //只能在订阅之后的才能收到
+        publishSubject.onNext(15);
+        publishSubject.onNext(16);
+    }
+
+    /**
+     * ReplaySubject会发射所有来自原始Observable的数据给观察者，无论它们是何时订阅的。也有其它版本的ReplaySubject，在重放缓存增长到一定大小的时候或过了一段时间后会丢弃旧的数据（原始Observable发射的）。
+
+     如果你把ReplaySubject当作一个观察者使用，注意不要从多个线程中调用它的onNext方法（包括其它的on系列方法），这可能导致同时（非顺序）调用，这会违反Observable协议，给Subject的结果增加了不确定性。
+     */
+    private void doReplySubject(){
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+        replaySubject.onNext("12");
+        replaySubject.onNext("23");
+        replaySubject.onNext("333");
+        replaySubject.subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.i(TAG,"doReplySubject "+s);
+
+            }
+        });
+        replaySubject.onNext("tianxia");
+        replaySubject.onNext("tege");
+    }
 
 }
